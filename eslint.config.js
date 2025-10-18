da4d2efcf51fe041a9bc3ddadecb84fde3f76faf
Andrii Minikh <andreoven@gmail.com>
2025-10-18 22:52:34 +0200

    add build config, eslint and prettier configs

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+import eslintConfigPrettier from 'eslint-config-prettier';
+
+const typedForSrc = tseslint.configs.recommendedTypeChecked.map((cfg) => ({
+  ...cfg,
+  files: ['src/**/*.ts'],
+}));
+
+export default tseslint.config(
+  { ignores: ['dist/**', 'node_modules/**'] },
+  js.configs.recommended,
+  ...typedForSrc,
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      'no-console': 'warn',
+      'no-debugger': 'error',
+      '@typescript-eslint/consistent-type-imports': ['warn', { prefer: 'type-imports' }],
+      '@typescript-eslint/no-unused-vars': ['warn'],
+    },
+  },
+  eslintConfigPrettier,
+);
