435eee6c5c3c253db809e40f56f499d5e954fe71
Andrii Minikh <andreoven@gmail.com>
2025-10-19 12:44:52 +0200

    add minimal implementation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,31 @@
-export const calc = (a: number, b: number) => {
-  return a + b;
+const createWorkerTemplate = (
+  funcString: string,
+) => `self.onmessage = async (e) => {
+  const execFunc = ${funcString}
+ 
+  const result = await execFunc(...e.data);
+  postMessage(result);
+};
+`;
+
+type WorkerFunction = (...args: unknown[]) => unknown;
+
+export const createWorker = (fn: WorkerFunction) => {
+  const toString = fn.toString();
+  const workerTemplate = createWorkerTemplate(toString);
+
+  const blob = new Blob([workerTemplate], { type: 'text/javascript' });
+  const blobURL = URL.createObjectURL(blob);
+  const myWorker = new Worker(blobURL);
+
+  const returnFunction = async (...args: unknown[]) => {
+    myWorker.postMessage([...args]);
+    return new Promise((resolve, reject) => {
+      myWorker.onmessage = (e) => {
+        resolve(e.data);
+      };
+    });
+  };
+
+  return returnFunction;
 };
